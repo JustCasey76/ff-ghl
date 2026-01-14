656476d667d58b2d663919026674f51f571b2806
Justin Casey <justi@aqmarketing.com>
2026-01-14 16:53:19 -0500

    v1.5.9 - Fix test button text encoding

--- aqm-ghl-connector.php
+++ aqm-ghl-connector.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: AQM GHL Formidable Connector
  * Description: Sends Formidable Forms submissions to GoHighLevel (LeadConnector) as Contacts using a Private Integration token.
- * Version: 1.5.8
+ * Version: 1.5.9
  * Author: AQMarketing
  */
 
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'AQM_GHL_CONNECTOR_VERSION', '1.5.8' );
+define( 'AQM_GHL_CONNECTOR_VERSION', '1.5.9' );
 define( 'AQM_GHL_CONNECTOR_DIR', plugin_dir_path( __FILE__ ) );
 define( 'AQM_GHL_CONNECTOR_URL', plugin_dir_url( __FILE__ ) );
 define( 'AQM_GHL_OPTION_KEY', 'aqm_ghl_connector_settings' );
